8489d84afa13024f09e4e09dfe8fa191fb883d6a
Belfast <1393037204@qq.com>
2021-11-22 20:53:44 +0800

    在App.java中输出master 3

--- src/main/java/howe/cookie/App.java
+++ src/main/java/howe/cookie/App.java
@@ -7,5 +7,6 @@ public class App {
     public static void main( String[] args ) {
         System.out.println("master 1");
         System.out.println("master 2");
+        System.out.println("master 3");
     }
 }
